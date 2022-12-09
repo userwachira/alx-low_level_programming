0a448308e493db71cc1a3e9adfb192d8e5687cac
Wachira <catherinewachira6@gmail.com>
2022-12-09 14:53:34 -0800

    Print all alpabets except two

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints all alphabets except q and e
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
+	putchar('\n');
+	return (0);
+}
