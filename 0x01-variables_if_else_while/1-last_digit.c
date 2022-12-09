93c91d96bf87ae3efff84da3ffff787f406b2c19
Wachira <catherinewachira6@gmail.com>
2022-12-09 11:12:20 -0800

    last digit arguement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,13 +19,13 @@ int main(void)
 	{
 		printf("greater than 5\n");
 	}
-	else if (last < 6 && !0)
+	else if (last == 0)
 	{
-		printf("less than 6 and not 0\n");
+		printf("0\n");
 	}
 	else
 	{
-		printf("0\n");
+		printf("less than 6 and not 0\n");
 	}
 	return (0);
 }
