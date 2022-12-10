817cc066a30e2b54b6e2e4472bf3a86beb206b76
Wachira <catherinewachira6@gmail.com>
2022-12-09 18:56:52 -0800

    Print all possible combinations of 2digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,7 +12,7 @@ int main(void)
 	{
 		for (j = i; j <= 99; j++)
 		{
-			if (j !=i)
+			if (j != i)
 			{
 				putchar(i / 10 + 48);
 				putchar(i % 10 + 48);
@@ -20,7 +20,7 @@ int main(void)
 				putchar(j / 10 + 48);
 				putchar(j % 10 + 48);
 
-				if (i * 100 + j ! = 9899)
+				if (i * 100 + j != 9899)
 				{
 					putchar(',');
 					putchar(' ');
