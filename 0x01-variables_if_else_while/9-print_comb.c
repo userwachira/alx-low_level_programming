2a4654d9f5ea85c8a8d708f8bdc337e5626c7764
Wachira <catherinewachira6@gmail.com>
2022-12-09 16:34:59 -0800

    Print combinations of single digit numbers with commas and spaces

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all combinations of single digit numbers with comman and space
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		putchar(i + 0);
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
