e1b267c13685d9408176356735bd143e1d710037
Wachira <catherinewachira6@gmail.com>
2022-12-16 00:56:28 -0800

    returns the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit
+ * @x: the last digit
+ * Return: the last digit
+ */
+int print_last_digit(int x)
+{
+	int x = y % 10;
+
+	return (x);
+}
