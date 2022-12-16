9fa518c6661464abafdda652b1cc44a109648f9c
Wachira <catherinewachira6@gmail.com>
2022-12-16 03:31:37 -0800

    print every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,9 +5,11 @@
  */
 void jack_bauer(void)
 {
-	for (int hour = 0; int hour < 24; hour++)
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
 	{
-		for (int minute = 0; int minute < 60; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{
 			_putchar("%02d:%02d\n", minute, hour);
 		}
