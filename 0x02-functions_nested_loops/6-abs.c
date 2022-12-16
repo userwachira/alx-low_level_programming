4816bc1d3512ce76adb027c4b33d442d00f58a26
Wachira <catherinewachira6@gmail.com>
2022-12-16 00:29:19 -0800

    absolute value of integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - prints the absolute value of an integer
+ * @int: the number
+ * Return: 0 success *
+ */
+int _abs(int)
+{
+	if (int < 0)
+	{
+		_putchar('int');
+	}
+	else
+	{
+		_putchar('int');
+	}
+}
