806fdb3ea03577074f6a2d6a63d3352fa34b95c6
Wachira <catherinewachira6@gmail.com>
2022-12-19 15:52:47 -0800

    reset pointer value

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,9 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value of the pointer
- * @n - the value
+ * @n: the pointer value
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
-
-	_putchar(*n);
-	_putchar('\n');
 }
