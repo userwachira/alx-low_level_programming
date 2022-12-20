4056bb35f0e4d1b2ef0f314944fbb71640c86a08
Wachira <catherinewachira6@gmail.com>
2022-12-19 17:01:41 -0800

    print string then newline

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string then a new line
+ * @str: the string
+ */
+void _puts(char *str)
+{
+	while (*str !='\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n')
+}
