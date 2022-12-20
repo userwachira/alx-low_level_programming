e5feecfc077f4e9f4e34397c94e3a17fc81737a2
Wachira <catherinewachira6@gmail.com>
2022-12-19 16:49:51 -0800

    length of the string

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,17 @@
 /**
  * _strlen - returns the length of the string
  * @s: the string
+ * Return: length of the string
  */
 int _strlen(char *s)
 {
 	int len = 0;
-	
+
 	while (*s != '\0')
 	{
 		len++;
 		s++;
 	}
+
+	return (len);
 }
