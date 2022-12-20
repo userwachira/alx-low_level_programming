152e09c3579b2ea3598a593c50b7c5d76a35437a
Wachira <catherinewachira6@gmail.com>
2022-12-19 17:18:09 -0800

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * print_rev - prints a string in reverse
- * @*s: the string
+ * @s: the string
  */
 void print_rev(char *s)
 {
 	int i;
-	
+
 	i = 0;
 	while (s[i] != '\0')
 	{
