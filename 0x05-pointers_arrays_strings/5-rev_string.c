0f20362e3ad572d0782ffab669a64f0a5ede895f
Wachira <catherinewachira6@gmail.com>
2022-12-19 17:42:39 -0800

    reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * rev_string - reverses the string
+ * @s: the string
+ * Return: reverse string
+ */
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int i = 0;
+	int j;
+
+	while (s[i] !='\0')
+		i++;
+	for (j = 0; j < i; j++)
+	{
+		i--;
+		rv = s[j];
+		s[j] = s[i];
+		s[i] = rv;
+	}
+}
