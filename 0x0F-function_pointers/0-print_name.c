9001e8453707b8f1a180a831cb9193b3fd51995e
Wachira <catherinewachira6@gmail.com>
2023-01-19 15:08:06 -0800

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.c"
+/**
+ * print_name - function that prints a name
+ * @name: name to print
+ * @f: pointer to function
+ */ 
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
