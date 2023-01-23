0865ad2215b42c889b8111baf80047fab24fc63f
Wachira <catherinewachira6@gmail.com>
2023-01-22 16:34:11 -0800

    structures typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
-struct dog
+#ifndef DOG_H_
+#define DOG_H_
+
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
