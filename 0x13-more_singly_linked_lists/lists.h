04c2490b24bd4a3092b67dfcea4f9ab4d904c5cd
Wachira <catherinewachira6@gmail.com>
2023-02-02 07:51:06 -0800

    update

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,14 +8,15 @@
  * struct listint_t - singly linked list
  * @n: integer 
  * @next: points to next node
- * description of linked list for
- * project
+ * 
+ * Description: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
