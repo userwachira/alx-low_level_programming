a14f91e2b51cf0a862dfd7b88de0c9ecc4161d4a
Wachira <catherinewachira6@gmail.com>
2023-02-02 08:03:40 -0800

    updates

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,16 +9,9 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		if (h->next == NULL)
-		{
-			printf("%d\n", h->n);
-		}
-		else
-		{
-			printf("%d\n", h->n);
-		}
+		printf("%d\n", h->n);
 		h = h->next;
 		nodes++;
 	}
